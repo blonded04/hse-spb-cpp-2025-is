1a96b9e9758391fd84a2222fc8207c5dd8ca8349
Valery Matskevich <generalretcher@gmail.com>
2026-02-24 20:01:20 +0300

    18-260224: correct statement for 20
    Signed-off-by: Valery Matskevich <generalretcher@gmail.com>

--- 18-260224/problem/20-read-object-as-bytes.cpp
+++ 18-260224/problem/20-read-object-as-bytes.cpp
@@ -17,12 +17,12 @@ int main() {
     static_assert(alignof(Point) == 1);
     static_assert(std::is_trivially_copyable_v<Point>);
 
-    Point *a = new (&data[1]) Point;
-    Point *b = new (&data[23]) Point;
-
     std::ifstream f("20-read-object-as-bytes.bin", std::ios_base::in | std::ios_base::binary);
     f.read(data, sizeof data);
     std::cout << std::hex;
+
+    Point *a = reinterpret_cast<Point*>(&data[1]);  // TODO, you can move this line
+    Point *b = reinterpret_cast<Point*>(&data[23]);  // TODO, you can move this line
     std::cout << "a = " << a->x << " " << a->y << std::endl;
     std::cout << "b = " << b->x << " " << b->y << std::endl;
 
@@ -31,6 +31,5 @@ int main() {
     assert(b->x == 0x34686769);
     assert(b->y == 0x72676835);
 
-    b->~Point();
-    a->~Point();
+    // TODO
 }
